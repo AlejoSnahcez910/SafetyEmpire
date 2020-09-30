2d4a988687f89a1989e68825db3b07ac4789c8a0
matzu19 <mateo.zuluagag@upb.edu.co>
2020-09-30 12:16:54 -0500

    unidad

--- Assets/Scripts/Unidad.cs
+++ Assets/Scripts/Unidad.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Unidad : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
     public GameObject ruta;
     private int indice;
-    private Vector3 posicion_inicial;
-    private Vector3 posicion_siguiente;
-    private float vel = 1.5f;
-    private float distancia_punto = 0.5f;
+    private Vector2 posicion_inicial;
+    private Transform posicion_siguiente;
+    private Transform posicion_actual;
+    private float vel;
+    private float distancia_punto;
     private bool esta_viva;
+    private float tiempo;
+    private int vidas;
+    private Vector3 posicion_muerte;
 
     public bool Esta_viva { get => esta_viva; set => esta_viva = value; }
 
@@ -24,9 +28,13 @@ public class Unidad : MonoBehaviour
     private void Start()
 
     {
+        vel = 2f;
+        vidas = 3;
+        distancia_punto = .1f;
         esta_viva = true;
         posicion_inicial = this.transform.position;
-        posicion_siguiente = ruta.transform.GetChild(0).position;
+        posicion_siguiente = ruta.transform.GetChild(0); 
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -44,26 +52,52 @@ public class Unidad : MonoBehaviour
 
         if (Esta_viva)
         {
-            Vector3 dir = posicion_siguiente - this.transform.position;
-            this.transform.position += dir * vel * Time.deltaTime;
+            //Vector3 dir = posicion_siguiente - this.transform.position;
+            // this.transform.position += dir * vel * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, posicion_siguiente.position, vel * Time.deltaTime);
 
-            if (dir.magnitude <= distancia_punto)
+            if (Vector2.Distance(transform.position,posicion_siguiente.position)<distancia_punto)
             {
                 if (indice + 1 < ruta.transform.childCount)
                 {
                     indice++;
-                    posicion_siguiente = ruta.transform.GetChild(indice).position;
-                    Debug.Log("xs" + posicion_siguiente.x + "ys" + posicion_siguiente.y);
+                    posicion_actual = posicion_siguiente;
+                    posicion_siguiente = ruta.transform.GetChild(indice);
+                   //CambiarPosicion();
                 }
                 else
                 {
                     indice = 0;
-                    this.transform.position = posicion_inicial;
-                    posicion_siguiente = ruta.transform.GetChild(0).position;
+                    transform.position = posicion_inicial;
+                    posicion_siguiente = ruta.transform.GetChild(0);
+                    posicion_actual = null;
+                    
                 }
             }
         }
        
+    }
+
+    private void CambiarPosicion()
+    {
+        
+        Direccion mira_hacia;
+
+        if(posicion_actual != null)
+        {
+            mira_hacia = posicion_actual.GetComponent<Direccion>();
+            if (mira_hacia.Ubicacion == Direccion.arriba)
+            {
+                
+            }
+
+
+
+        }
+
+
+
+
     }
 
 }
