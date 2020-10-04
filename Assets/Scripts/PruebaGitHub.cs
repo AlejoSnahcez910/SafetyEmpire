619d8832af68c732203d6779d9bcd51fb0844632
matzu19 <mateo.zuluagag@upb.edu.co>
2020-10-04 18:52:52 -0500

    int en prueba

--- Assets/Scripts/PruebaGitHub.cs
+++ Assets/Scripts/PruebaGitHub.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Alejandro : MonoBehaviour
 {
+    public int numero;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
