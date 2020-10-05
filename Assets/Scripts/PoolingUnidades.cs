e6ea153268ea2d071877a853a7fbd1fe499559ee
matzu19 <mateo.zuluagag@upb.edu.co>
2020-10-04 21:34:44 -0500

    Unidades2y3

--- Assets/Scripts/PoolingUnidades.cs
+++ Assets/Scripts/PoolingUnidades.cs
@@ -9,12 +9,26 @@ public class PoolingUnidades : MonoBehaviour
     void Start()
     {
         GameObject enemigo = GameObject.Find("Enemigo1");
+        GameObject enemigo2 = GameObject.Find("Enemigo2");
+        GameObject enemigo3 = GameObject.Find("Enemigo3");
         GameObject temp;
         Vector3 incremento = new Vector3(-1, 0);
         Vector3 posicion_actual = enemigo.transform.position;
+        enemigos = new ArrayList();
+        enemigos.Add(enemigo);
+        enemigos.Add(enemigo2);
+        enemigos.Add(enemigo3);
         for (int i = 0; i < 10; i++)
         {
             temp = (GameObject)Instantiate(enemigo, posicion_actual + incremento, Quaternion.identity);
+            posicion_actual = temp.transform.position;
+            enemigos.Add(temp);
+
+            temp = (GameObject)Instantiate(enemigo2, posicion_actual + incremento, Quaternion.identity);
+            posicion_actual = temp.transform.position;
+            enemigos.Add(temp);
+
+            temp = (GameObject)Instantiate(enemigo3, posicion_actual + incremento, Quaternion.identity);
             posicion_actual = temp.transform.position;
             enemigos.Add(temp);
         }
