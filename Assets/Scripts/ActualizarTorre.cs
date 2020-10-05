a2992b14a6889c686e8215df8273c2468c967fbd
matzu19 <mateo.zuluagag@upb.edu.co>
2020-10-04 23:41:20 -0500

    ControlesTorre

---
+++ Assets/Scripts/ActualizarTorre.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualizarTorre : MonoBehaviour
+{
+    private ControlesTorre ct;
+    private Animator estados;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ct = this.GetComponentInParent<ControlesTorre>();
+        estados = this.GetComponentInParent<Animator>();
+    }
+    private void OnMouseDown()
+    {
+        estados.SetInteger("nivel", 1);
+        ct.CambiarEstadoBotones(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ControlesTorre.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlesTorre : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject botonActualizar;
+    [SerializeField]
+    private GameObject BotonVender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CambiarEstadoBotones(false);
+    }
+    private void OnMouseDown()
+    {
+        CambiarEstadoBotones(true);
+    }
+    public void CambiarEstadoBotones (bool estado)
+    {
+        botonActualizar.SetActive(estado);
+        BotonVender.SetActive(estado);
+    }
+    // Update is called once per frame
+   
+}
